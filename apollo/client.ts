64e656a4b428f12cdf81352cf234c5941d8e31bd
Dan Ward <danward@gmail.com>
2022-07-14 16:15:13 -0700

    Improve navbar

--- apollo/client.ts
+++ apollo/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://hasura-0zkd.onrender.com/v1/graphql",
+  uri: process.env.GRAPHQL_API_ENDPOINT,
   cache: new InMemoryCache(),
 });
 
