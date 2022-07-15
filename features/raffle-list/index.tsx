b708a3ad66951d3e3bf2e1faa81568101a537f1a
Dan Ward <danward@gmail.com>
2022-07-14 17:45:45 -0700

    Move raffle item data into props in parent

--- features/raffle-list/index.tsx
+++ features/raffle-list/index.tsx
@@ -1,13 +1,23 @@
 import RaffleListItem from "features/raffle-list/raffle-list-item";
 
+const items = [
+  {
+    imgSrc: "https://www.fillmurray.com/250/250",
+    name: "Name of NFT",
+    endTime: "1 day, 2 hours",
+    totalTicketCount: 4200,
+    totalSoldTicketCount: 420,
+    totalUserTicketCount: 4,
+    pricePerTicketInGoods: 2,
+  },
+];
+
 export const RaffleList = () => {
   return (
     <div className="grid grid-cols-3 gap-8">
-      <RaffleListItem />
-      <RaffleListItem />
-      <RaffleListItem />
-      <RaffleListItem />
-      <RaffleListItem />
+      {items.map((item, i) => (
+        <RaffleListItem key={i} item={items[0]} />
+      ))}
     </div>
   );
 };
--- features/raffle-list/raffle-list-item/index.tsx
+++ features/raffle-list/raffle-list-item/index.tsx
@@ -1,32 +1,43 @@
 import Image from "next/image";
 
-export const RaffleListItem = () => {
+type Props = {
+  item: any;
+};
+
+export const RaffleListItem = ({ item }: Props) => {
+  const {
+    name,
+    endTime,
+    totalTicketCount,
+    totalUserTicketCount,
+    totalSoldTicketCount,
+    pricePerTicketInGoods,
+    imgSrc,
+  } = item;
   return (
     <div className="flex flex-col w-full p-3 bg-amber-200 border-black border-2 space-y-2">
-      <Image
-        height={250}
-        width={250}
-        src="https://www.fillmurray.com/250/250"
-        alt="raffle item"
-      />
-      <div className="text-2xl font-bold">Name of NFT</div>
+      <Image height={250} width={250} src={imgSrc} alt="raffle item" />
       <div>
         <div className="text-lg text-green-800 font-semibold">Ends in</div>
-        <div className="text-lg font-bold">Name of NFT</div>
+        <div className="text-lg font-bold">{endTime}</div>
+      </div>
+      <div>
+        <div className="text-lg text-green-800 font-semibold">Your tickets</div>
+        <div className="text-lg font-bold">{totalUserTicketCount}</div>
       </div>
       <div>
         <div className="text-lg text-green-800 font-semibold">Sold Tickets</div>
-        <div className="text-lg font-bold">42</div>
+        <div className="text-lg font-bold">{totalSoldTicketCount}</div>
       </div>
       <div>
         <div className="text-lg text-green-800 font-semibold">
           Total Tickets
         </div>
-        <div className="text-lg font-bold">4200</div>
+        <div className="text-lg font-bold">{totalTicketCount}</div>
       </div>
       <div className="pb-3">
         <div className="text-lg text-green-800 font-semibold">Ticket Price</div>
-        <div className="text-lg font-bold">42 $GOODS</div>
+        <div className="text-lg font-bold">{pricePerTicketInGoods} $GOODS</div>
       </div>
       <button className="w-full py-3 bg-red-600 text-amber-200 uppercase rounded-lg">
         Join Raffle
