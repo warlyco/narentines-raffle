6277622489630cd35e64807be7640869a4f13436
Dan Ward <danward@gmail.com>
2022-07-14 17:50:00 -0700

    Redirect index to raffles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/raffle");
+  }, []);
+
   return (
     <div className="">
       <div className="max-w-6xl m-auto p-4 py-6 text-2xl">NARENTINES</div>
