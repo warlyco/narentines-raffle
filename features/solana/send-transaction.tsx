768b1ea4c180aaeefd74232f7545f03a4fcf9eb9
Dan Ward <danward@gmail.com>
2022-07-19 20:28:55 -0700

    Add working send transaction

--- features/solana/send-transaction.tsx
+++ features/solana/send-transaction.tsx
@@ -1,6 +1,7 @@
 import { useMutation } from "@apollo/client";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import {
+  Keypair,
   LAMPORTS_PER_SOL,
   PublicKey,
   SystemProgram,
@@ -14,9 +15,15 @@ import toast from "react-hot-toast";
 import classNames from "classnames";
 import { Action, Dispatch } from "redux";
 import { Raffle } from "types";
-import { TOKEN_PROGRAM_ID, createTransferInstruction } from "@solana/spl-token";
+import {
+  TOKEN_PROGRAM_ID,
+  createTransferInstruction,
+  getOrCreateAssociatedTokenAccount,
+  createTransferCheckedInstruction,
+  createMint,
+} from "@solana/spl-token";
 
-const { NEXT_PUBLIC_COLLECTION_WALLET } = process.env;
+import bs58 from "bs58";
 
 type Props = {
   raffle: Raffle;
@@ -34,7 +41,7 @@ export const SendTransaction = ({
   raffleIsOver,
 }: Props) => {
   const { connection } = useConnection();
-  const { publicKey, sendTransaction } = useWallet();
+  const { publicKey: fromPublicKey, signTransaction } = useWallet();
   const [addRaffleEntry, { data, loading, error }] = useMutation(
     ADD_RAFFLE_ENTRY,
     {
@@ -43,7 +50,7 @@ export const SendTransaction = ({
           query: GET_ENTRIES_COUNT,
           variables: {
             raffleId: raffle.id,
-            walletAddress: publicKey?.toString(),
+            walletAddress: fromPublicKey?.toString(),
           },
         },
       ],
@@ -51,46 +58,96 @@ export const SendTransaction = ({
   );
 
   const onClick = useCallback(async () => {
-    if (!publicKey || !NEXT_PUBLIC_COLLECTION_WALLET) {
+    if (!fromPublicKey || !signTransaction) {
       console.log("error", "Wallet not connected!");
+      return;
+    }
+    if (
+      !process.env.NEXT_PUBLIC_GOODS_TOKEN_ADDRESS ||
+      !process.env.NEXT_PUBLIC_COLLECTION_WALLET ||
+      !process.env.NEXT_PUBLIC_GOODS_TOKEN_MINT_ADDRESS ||
+      !process.env.NEXT_PUBLIC_USDC_TOKEN_ADDRESS ||
+      !process.env.NEXT_PUBLIC_COLLECTION_WALLET
+    ) {
+      console.log("error", "Config error");
       return;
     }
 
     let signature: TransactionSignature = "";
     try {
       // SOL
-      const solInLamports =
-        (LAMPORTS_PER_SOL / 100) * Number(numberOfTicketsToBuy);
+      // const solInLamports =
+      //   (LAMPORTS_PER_SOL / 100) * Number(numberOfTicketsToBuy);
 
-      const transaction = new Transaction().add(
-        SystemProgram.transfer({
-          fromPubkey: publicKey,
-          toPubkey: new PublicKey(NEXT_PUBLIC_COLLECTION_WALLET),
-          lamports: solInLamports,
-        })
-      );
-
-      // SPL TOKEN
       // const transaction = new Transaction().add(
-      //   createTransferInstruction(
-      //     publicKey,
-      //     new PublicKey(NEXT_PUBLIC_COLLECTION_WALLET),
-      //     publicKey,
-      //     Number(numberOfTicketsToBuy),
-      //     [],
-      //     TOKEN_PROGRAM_ID
-      //   )
+      //   SystemProgram.transfer({
+      //     fromPubkey: publicKey,
+      //     toPubkey: new PublicKey(NEXT_PUBLIC_COLLECTION_WALLET),
+      //     lamports: solInLamports,
+      //   })
       // );
 
-      signature = await sendTransaction(transaction, connection);
-      console.log("info", "Transaction sent:", signature);
+      // SPL TOKEN
+
+      const toAddress = process.env.NEXT_PUBLIC_COLLECTION_WALLET;
+      const toPublicKey = new PublicKey(toAddress);
+      const amount = Number(numberOfTicketsToBuy) * raffle.priceInGoods;
+
+      const tokenPublicKey = new PublicKey(
+        process.env.NEXT_PUBLIC_GOODS_TOKEN_MINT_ADDRESS
+      );
+
+      const fromTokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        fromPublicKey,
+        tokenPublicKey,
+        fromPublicKey,
+        signTransaction
+      );
+      console.log(fromTokenAccount);
+
+      const toTokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        fromPublicKey,
+        tokenPublicKey,
+        toPublicKey,
+        signTransaction
+      );
+
+      const transaction = new Transaction().add(
+        createTransferInstruction(
+          // imported from '@solana/spl-token'
+          fromTokenAccount.address,
+          toTokenAccount.address,
+          fromPublicKey,
+          amount, // tokens have 6 decimals of precision so your amount needs to have the same
+          [],
+          TOKEN_PROGRAM_ID // imported from '@solana/spl-token'
+        )
+      );
+
+      const latestBlockHash = await connection.getLatestBlockhash();
+      transaction.recentBlockhash = latestBlockHash.blockhash;
+
+      // set who is the fee payer for that transaction
+      transaction.feePayer = fromPublicKey;
+
+      // const { signatures } = await signTransaction(transaction);
+      const signed = await signTransaction(transaction);
+
+      debugger;
+      console.log("info", "Transaction sent:", signed);
+      const signature = await connection.sendRawTransaction(signed.serialize());
       toast.custom(
         <div className="flex bg-white rounded-xl shadow-lg p-3 border-slate-400">
           <div>Transaction sent...</div>
         </div>
       );
-
-      await connection.confirmTransaction(signature, "processed");
+      await connection.confirmTransaction({
+        signature,
+        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+        blockhash: latestBlockHash.blockhash,
+      });
       console.log("success", "Transaction successful!", signature);
       toast.custom(
         <div className="flex bg-white rounded-xl shadow-xl p-3 border-slate-400">
@@ -108,7 +165,7 @@ export const SendTransaction = ({
 
       addRaffleEntry({
         variables: {
-          walletAddress: publicKey.toString(),
+          walletAddress: fromPublicKey.toString(),
           count: entryCount + Number(numberOfTicketsToBuy),
           soldTicketCount:
             raffle.soldTicketCount + Number(numberOfTicketsToBuy),
@@ -122,12 +179,12 @@ export const SendTransaction = ({
       setNumberOfTicketsToBuy(0);
     }
   }, [
-    publicKey,
-    numberOfTicketsToBuy,
-    sendTransaction,
+    fromPublicKey,
+    signTransaction,
     connection,
     addRaffleEntry,
     entryCount,
+    numberOfTicketsToBuy,
     raffle.soldTicketCount,
     raffle.id,
     setNumberOfTicketsToBuy,
@@ -151,7 +208,7 @@ export const SendTransaction = ({
       onClick={onClick}
       disabled={
         raffleIsOver ||
-        !publicKey ||
+        !fromPublicKey ||
         loading ||
         Number(numberOfTicketsToBuy) <= 0 ||
         Number(numberOfTicketsToBuy) >
@@ -163,7 +220,7 @@ export const SendTransaction = ({
         "border border-green-800 text-green-800": raffleIsOver,
         "opacity-80 cursor-not-allowed":
           raffleIsOver ||
-          !publicKey ||
+          !fromPublicKey ||
           loading ||
           Number(numberOfTicketsToBuy) <= 0 ||
           Number(numberOfTicketsToBuy) >
