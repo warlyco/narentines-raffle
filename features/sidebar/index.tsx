45f872b04b79a44560e0fb90c79a0c1bea2df841
Dan Ward <danward@gmail.com>
2022-07-21 23:38:38 -0700

    Add background to sidebar

--- features/sidebar/index.tsx
+++ features/sidebar/index.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import bg from "public/images/bg-black.png";
 
 type Props = {
   isOpenSidebar: boolean;
@@ -17,20 +18,23 @@ export const Sidebar = ({ isOpenSidebar, toggleSidebar }: Props) => {
       <div
         onClick={handleCloseSidebar}
         className={classNames({
-          "absolute top-0 right-0 bottom-0 left-0 transition-opacity duration-300 ease-in-out opacity-40 bg-slate-600":
-            true,
+          "absolute top-0 right-0 bottom-0 left-0 transition-opacity duration-300 ease-in-out opacity-40 bg-slate-800":
+            isOpenSidebar,
           "opacity-0 pointer-events-none": !isOpenSidebar,
         })}
       ></div>
       <div
         className={classNames({
-          "absolute top-0 right-0 bottom-0 w-[400px] h-screen transition-position duration-300 ease-in-out":
+          "absolute top-0 right-0 bottom-0 w-[380px] h-screen transition-position duration-300 ease-in-out":
             true,
           "-mr-96": !isOpenSidebar,
         })}
       >
-        <div className="p-4 h-full">
-          <div className="border bg-slate-100 h-full rounded shadow-2xl">
+        <div className="p-2 h-full">
+          <div
+            className="h-full rounded-md shadow-2xl"
+            style={{ backgroundImage: `url(${bg.src})` }}
+          >
             {String(isOpenSidebar)}
           </div>
         </div>
