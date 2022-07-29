35d0f0dd7e57f227348d9f0e2a1e3c133a478b50
Dan Ward <danward@gmail.com>
2022-07-28 18:22:39 -0700

    Add sentry to endpoints

--- pages/api/get-raffles/[id].ts
+++ pages/api/get-raffles/[id].ts
@@ -2,6 +2,17 @@ import type { NextApiHandler } from "next";
 import client from "apollo/client";
 import { GET_RAFFLE_BY_ID } from "graphql/queries/get-raffle-by-id";
 
+import * as Sentry from "@sentry/node";
+
+Sentry.init({
+  dsn: "https://f28cee1f60984817b329898220a049bb@o1338574.ingest.sentry.io/6609786",
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 const getRaffles: NextApiHandler = async (request, response) => {
   const { id } = request.query;
   try {
@@ -18,6 +29,7 @@ const getRaffles: NextApiHandler = async (request, response) => {
 
     response.json({ raffle });
   } catch (error) {
+    Sentry.captureException(error);
     response.status(500).json({ error });
   }
 };
--- pages/api/get-raffles/index.ts
+++ pages/api/get-raffles/index.ts
@@ -1,6 +1,16 @@
 import type { NextApiHandler } from "next";
 import client from "apollo/client";
 import { GET_RAFFLES } from "graphql/queries/get-raffles";
+import * as Sentry from "@sentry/node";
+
+Sentry.init({
+  dsn: "https://f28cee1f60984817b329898220a049bb@o1338574.ingest.sentry.io/6609786",
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 const getRaffles: NextApiHandler = async (request, response) => {
   try {
@@ -10,6 +20,7 @@ const getRaffles: NextApiHandler = async (request, response) => {
 
     response.json({ raffles: data.raffles });
   } catch (error) {
+    Sentry.captureException(error);
     response.status(500).json({ error });
   }
 };
