7a72bd12cf182434e52160695eadc0316d6d11b4
Dan Ward <danward@gmail.com>
2022-07-29 19:24:43 -0700

    Refactor how graph client is initd

--- pages/api/get-raffles/index.ts
+++ pages/api/get-raffles/index.ts
@@ -1,7 +1,7 @@
 import type { NextApiHandler } from "next";
 import { GET_RAFFLES } from "graphql/queries/get-raffles";
 import * as Sentry from "@sentry/node";
-import { GraphQLClient } from "graphql-request";
+import { request } from "graphql-request";
 
 Sentry.init({
   dsn: "https://f28cee1f60984817b329898220a049bb@o1338574.ingest.sentry.io/6609786",
@@ -13,19 +13,18 @@ Sentry.init({
 });
 
 const getRaffles: NextApiHandler = async (_, response) => {
-  const client = new GraphQLClient(
-    process.env.NEXT_PUBLIC_ADMIN_GRAPHQL_API_ENDPOINT!,
-    {
-      headers: {
-        "x-hasura-admin-secret": process.env.HASURA_GRAPHQL_ADMIN_SECRET!,
-      },
-    }
-  );
+  Sentry.captureMessage("getRaffles");
 
   try {
-    const { raffles } = await client.request(GET_RAFFLES);
+    const res = await request({
+      url: process.env.NEXT_PUBLIC_ADMIN_GRAPHQL_API_ENDPOINT!,
+      document: GET_RAFFLES,
+      requestHeaders: {
+        "x-hasura-admin-secret": process.env.HASURA_GRAPHQL_ADMIN_SECRET!,
+      },
+    });
 
-    response.json({ raffles });
+    response.json({ raffles: res.raffles });
   } catch (error) {
     Sentry.captureException(error);
     response.status(500).json({ error });
