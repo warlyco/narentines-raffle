448420a8f1fafe6c043306da680d5cc91c607a49
Dan Ward <danward@gmail.com>
2022-07-29 19:50:46 -0700

    Update logging

--- pages/api/get-raffles/index.ts
+++ pages/api/get-raffles/index.ts
@@ -17,12 +17,10 @@ const getRaffles: NextApiHandler = async (_, response) => {
     Sentry.captureMessage(
       JSON.stringify({
         url: process.env.NEXT_PUBLIC_ADMIN_GRAPHQL_API_ENDPOINT!,
-        document: GET_RAFFLES,
-        requestHeaders: {
-          "x-hasura-admin-secret": process.env.HASURA_GRAPHQL_ADMIN_SECRET!,
-        },
+        "x-hasura-admin-secret": process.env.HASURA_GRAPHQL_ADMIN_SECRET!,
       })
     );
+
     const res = await request({
       url: process.env.NEXT_PUBLIC_ADMIN_GRAPHQL_API_ENDPOINT!,
       document: GET_RAFFLES,
