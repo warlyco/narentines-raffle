3ef63b98c1e65c454ad361679b2ad7370760ff7b
Dan Ward <danward@gmail.com>
2022-08-05 18:34:38 -0700

    Add query to filter out test raffles when fetching raffles

--- graphql/queries/get-raffles.tsx
+++ graphql/queries/get-raffles.tsx
@@ -3,7 +3,10 @@ import { gql } from "@apollo/client";
 
 export const GET_RAFFLES = gql`
   query Raffles {
-    raffles(where: { isArchived: { _eq: false } }, order_by: { endsAt: desc }) {
+    raffles(
+      where: { isArchived: { _eq: false }, isTestRaffle: { _eq: false } }
+      order_by: { endsAt: desc }
+    ) {
       id
       name
       mintAddress
