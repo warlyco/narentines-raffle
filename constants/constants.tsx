a72baea605eb2d3759664ee13999120b96ba7f7e
Dan Ward <danward@gmail.com>
2022-09-07 21:02:32 -0700

    Add mint addresses

--- constants/constants.tsx
+++ constants/constants.tsx
@@ -7,8 +7,11 @@ export const RPC_ENDPOINT: string = process.env.NEXT_PUBLIC_RPC_ENDPOINT || "";
 export const SOLANA_CLUSTER = process.env.NEXT_PUBLIC_SOLANA_CLUSTER || "";
 export const SENTRY_TRACE_SAMPLE_RATE = 0.3;
 export const MINT_ADDRESSES = {
-  [SplTokens.SOL]: "11111111111111111111111111111111",
+  [SplTokens.SOL]: "11111111111111loca111111111111111111",
   [SplTokens.GOODS]: "C1FfuXjR9xZxJMNSMW3YRVEdLQhso1HUWRAZjtPbx4Rm",
+  [SplTokens.GEAR]: "7s6NLX42eURZfpyuKkVLrr9ED9hJE8718cyXFsYKqq5g",
+  [SplTokens.FORGE]: "FoRGERiW7odcCBGU1bztZi16osPBHjxharvDathL5eds",
+  [SplTokens.DUST]: "DUSTawucrTsGU8hcqRdHDCbuYhCPADMLM2VcCb8VnFnQ",
 };
 
 export const isProduction = process.env.NEXT_PUBLIC_ENV === "production";
